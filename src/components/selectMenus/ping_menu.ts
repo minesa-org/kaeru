6498bbcb2508b1776d39815650f0a268c2ed6058
İbrahim <neodevils_contact@icloud.com>
2026-02-26 12:49:15 +0300

    Refactor emoji handling in announce command
    - Updated emoji reactions to align with new IDs and names.
    - Improved overall structure and readability of the announce command code.

---
+++ src/components/selectMenus/ping_menu.ts
@@ -0,0 +1,18 @@
+import {
+    type ComponentCommand,
+    type StringSelectInteraction,
+} from "@minesa-org/mini-interaction";
+
+const selectmenu_menu: ComponentCommand = {
+    customId: "ping_menu",
+
+    handler: async (interaction: StringSelectInteraction) => {
+        const value = interaction.getStringValues().join(", ");
+
+        return interaction.reply({
+            content: `Value(s) of select menu you selected: ${value}`,
+        });
+    },
+};
+
+export default selectmenu_menu;
